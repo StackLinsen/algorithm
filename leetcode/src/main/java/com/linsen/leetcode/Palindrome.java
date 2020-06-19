009cd19cb02f765c751b3790d65ea242047a1e83
lane <lane.lin@ly.com>
2020-06-19 17:11:20 +0800

    2020-06-19 回文字符串

---
+++ leetcode/src/main/java/com/linsen/leetcode/Palindrome.java
@@ -0,0 +1,30 @@
+package com.linsen.leetcode;
+
+/**
+ * @author lane.lin
+ * @Description 判断回文字符串
+ * @since 2020/6/19
+ */
+public class Palindrome {
+
+    public boolean solution(String s){
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                ++left;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                --right;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
+}
